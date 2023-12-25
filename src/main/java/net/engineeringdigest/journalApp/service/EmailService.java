0c7d1da5eb5a74a8d01c13e7636b48e15de2b16d
chotabheeeeem <152039138+chotabheeeeem@users.noreply.github.com>
2023-12-25 20:15:47 +0530

    adds email service

---
+++ src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
@@ -0,0 +1,29 @@
+package net.engineeringdigest.journalApp.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(to);
+            mail.setSubject(subject);
+            mail.setText(body);
+            javaMailSender.send(mail);
+        } catch (Exception e) {
+            log.error("Exception while sendEmail ", e);
+        }
+    }
+
+
+}
---
+++ src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTests.java
@@ -0,0 +1,19 @@
+package net.engineeringdigest.journalApp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTests {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void testSendMail() {
+        emailService.sendEmail("carnation_duchess348@slmail.me",
+                "Testing Java mail sender",
+                "Hi, aap kaise hain ?");
+    }
+}
