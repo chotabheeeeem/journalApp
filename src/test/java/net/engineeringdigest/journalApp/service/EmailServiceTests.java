5b13713bfe9634a5d6eed6040a14be9e4c3aa65b
chotabheeeeem <152039138+chotabheeeeem@users.noreply.github.com>
2023-12-25 20:16:20 +0530

    adds email service

--- src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTests.java
@@ -1,5 +1,6 @@
 package net.engineeringdigest.journalApp.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ public class EmailServiceTests {
     @Autowired
     private EmailService emailService;
 
+    @Disabled
     @Test
     void testSendMail() {
         emailService.sendEmail("carnation_duchess348@slmail.me",
