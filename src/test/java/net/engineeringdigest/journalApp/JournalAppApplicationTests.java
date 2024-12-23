78b28212c22569f1ff6d9e78f72ac91b75bf7e7b
Ankesh <ankeshgang@gmail.com>
2024-12-23 13:24:47 +0530

    adding space for test commit

--- src/test/java/net/engineeringdigest/journalApp/JournalAppApplicationTests.java
+++ src/test/java/net/engineeringdigest/journalApp/JournalAppApplicationTests.java
@@ -3,6 +3,7 @@ package net.engineeringdigest.journalApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class JournalAppApplicationTests {
 
